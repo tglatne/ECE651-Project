3cfd25e79e634069e3fdf016af916d22e8bee706
Gabrielle Quan <qwy0129@gmail.com>
2022-01-25 22:36:01 -0500

    Redux: getAllProducts

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import ProductService from '../api/product/ProductService';
-import Product from '../components/Product';
-import { Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { Row, Col } from "react-bootstrap";
+import { listProducts } from "../actionCreators/productActionCreators";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+
   useEffect(() => {
-    ProductService.getAllProducts().then((response) =>
-      setProducts(response.data)
-    );
-  });
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
-    <div className='mt-4'>
-      <Row>
-        {products.map((product) => (
-          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+    <div className="mt-4">
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
