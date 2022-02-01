fd848dd481534be10a50d0d475b03cd6d9f27f00
Sagar <major1819@gmail.com>
2022-01-31 23:45:20 -0500

    category api created

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.getRoutes, name = 'routes'),
     path('products/', views.getProducts, name = 'products'),
     path('products/<str:pk>', views.getProduct, name = 'product'),
+    path('categories/', views.getCategories, name = 'categories'),
+    path('categories/<str:pk>', views.getCategory, name = 'cateogry')
+
     # path('', include(router.urls)),
 
 ]
