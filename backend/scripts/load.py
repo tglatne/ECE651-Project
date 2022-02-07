ba81545ba7637f4d1ef56aa54aaa346c6264e856
Sagar <major1819@gmail.com>
2022-02-07 11:26:32 -0500

    Load database in postgres from csv files

---
+++ backend/scripts/load.py
@@ -0,0 +1,17 @@
+from product.models import Product
+import csv
+import os
+def run():
+    file = open('D:/Waterloo/Winter 22/ECE 651/ECE651-Project/backend/scripts/Data ECE651 - Meat & Seafood.csv')
+    read_file = csv.reader(file)
+    # Product.objects.all().delete()
+    count = 1
+    for record in read_file:
+        if count ==1:
+            pass
+        else:
+            # print("hi")
+            Product.objects.create( product_name = record[0], category_id = record[1], price_walmart = record[2],
+                           price_sobeys = record[3], price_zehrs = record[4], image = record[5],walmart_url = record[6],
+                           sobeys_url = record[7], zehrs_url = record[8], description = record[9])
+        count+=1
