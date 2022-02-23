f4660c65e548b8d857d2ab870796284e296ad0c0
Sagar <major1819@gmail.com>
2022-02-22 22:05:17 -0500

    products getting updated automicatically

--- backend/product/tasks.py
+++ backend/product/tasks.py
@@ -8,25 +8,26 @@ import time
 
 @shared_task(bind=True)
 def my_test(self):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--enable-javascript")
-    driver = webdriver.Chrome('D:/Waterloo/Winter 22/ECE 651/ECE651-Project/backend/product/chromedriver.exe', options=options)
     products = Product.objects.all()
     for product in products.iterator():
-
-        url_walmart = str(product.walmart_url)
-        url_sobeys = str(product.sobeys_url)
-        url_zehrs = str(product.zehrs_url)
         class_walmart = 'css-2vqe5n esdkp3p0'
         class_sobeys = 'text__Text-sc-1ddlex6-0 hgqPwo'
         class_zehrs = 'price__value selling-price-list__item__price selling-price-list__item__price--now-price__value'
-        url_class_dict = {url_walmart : class_walmart, url_sobeys : class_sobeys, url_zehrs : class_zehrs}
-        for key in range(len(url_class_dict)):
-            print(key)
-            print(type(key))
+        url_class_dict = {product.walmart_url : class_walmart, product.sobeys_url : class_sobeys, product.zehrs_url : class_zehrs}
+
+        for key in url_class_dict.keys():
+            options = webdriver.ChromeOptions()
+            options.add_argument("--enable-javascript")
+            driver = webdriver.Chrome('D:/Waterloo/Winter 22/ECE 651/ECE651-Project/backend/product/chromedriver.exe',
+                                      options=options)
             driver.get(key)
             time.sleep(5)
+            html = driver.page_source
             soup = BeautifulSoup(html, 'lxml')
+            soup_str = str(soup)
+            if 'text__Text-sc-1ddlex6-0 offer__Offer-sc-1atj9an-0 hgqPwo eZJjBz' in soup_str:
+                print("hi")
+                url_class_dict[product.sobeys_url] = 'text__Text-sc-1ddlex6-0 offer__Offer-sc-1atj9an-0 hgqPwo eZJjBz'
             soup = soup.find('span', class_= url_class_dict[key])
             price = soup.text
             price = str(price)
@@ -38,6 +39,18 @@ def my_test(self):
                 price = float(price) / 100
             print(key," ", price )
             driver.quit()
+            if 'walmart' in key:
+                Product.objects.filter(product_name = product.product_name).update(price_walmart = price)
+
+            elif 'zehrs' in key:
+                Product.objects.filter(product_name = product.product_name).update(price_zehrs = price)
+
+            elif 'voila' in key:
+                Product.objects.filter(product_name = product.product_name).update(price_sobeys = price)
+
+
+
+
 
 
 
